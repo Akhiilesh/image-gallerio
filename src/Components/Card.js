e6c21bb35857bdb12debd9f95c630b5e32fa49ee
Akhilesh Sanap <akhileshsanap091@gmail.com>
2024-08-27 17:44:20 +0530

    Organize components and exclued some options from the navbar

---
+++ src/Components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div className="col mb-5">
+      <div class="card" style={{ width: "18rem" }}>
+        <img
+          src="https://i.pinimg.com/564x/e7/b3/b4/e7b3b467993d76e3e62fc1fd18d404cb.jpg"
+          class="card-img-top"
+          alt="image"
+        />
+      </div>
+    </div>
+  );
+}
